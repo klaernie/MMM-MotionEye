ded57353359a301be8eef7461240e1ce44f77f1a
Andre Klärner <kandre@ak-online.be>
2022-05-01 15:10:26 +0200

    setup show/hide routes at module start
    setting them up when receiving the CONFIG notification doesn't seem to
    work anymore.

--- node_helper.js
+++ node_helper.js
@@ -10,27 +10,27 @@ var NodeHelper = require("node_helper");
 module.exports = NodeHelper.create({
 	start: function() {
 		console.log("Starting module: " + this.name);
+
+		var self = this;
+		this.expressApp.get('/motioneye/hide/:id*?', function (req, res) {
+			console.log("Hide registered: " + req.params.id);
+			res.send('Hide registered: ' + req.params.id);
+			self.sendSocketNotification("MotionEyeHide", req.params.id);
+		});
+		this.expressApp.get('/motioneye/:id*?', function (req, res) {
+			console.log("Motion registered: " + req.params.id);
+			res.send('Motion registered: ' + req.params.id);
+			self.sendSocketNotification("MotionEyeShow", req.params.id);
+		});
 	},
 
 	socketNotificationReceived: function(notification, config) {
 		if (notification === "CONFIG") {
+			console.log("started configuring the node helper of " + this.name);
 			if (config.autoHide) {
-				var self = this;
-				
 				console.log("Hiding camera: " + config.id);
 				this.sendSocketNotification("MotionEyeHide", config.id);
-
-				this.expressApp.get('/motioneye/hide/:id*?', function (req, res) {
-					console.log("Hide registered: " + req.params.id);
-					res.send('Hide registered: ' + req.params.id);
-					self.sendSocketNotification("MotionEyeHide", req.params.id);
-				});
-				this.expressApp.get('/motioneye/:id*?', function (req, res) {
-					console.log("Motion registered: " + req.params.id);
-					res.send('Motion registered: ' + req.params.id);
-					self.sendSocketNotification("MotionEyeShow", req.params.id);
-				});
-			} 
+			}
 
 			return;
 		}
